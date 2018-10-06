54144b9946790f64d9b98cb50c655e0cab6c4721
Dimitri Zangana <dimitri@nregames.co.uk>
2018-10-06 14:54:51 +0300

    4. (Main Menu Systems) The scripts works as a skippable intro scene via 'CreditFeatures'. The 'MainMenuCamera' script works as the core mechanic of the main menu. The 'SelectCharacter' script works as a script that allows the player to select a character model when creating their character on the Main Menu.

---
+++ 4. Main Menu Systems/CreditFeatures.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CreditFeatures : MonoBehaviour
+{
+    public CanvasGroup cg;
+    public bool canSkip;
+    public bool isSkipped;
+    public bool isDone;
+    public bool begin;
+
+    //This script was created for the purpose of skipping the intro parts before having access to the main menu.
+   
+    void Start()
+    {
+        cg.alpha = 0;
+        StartCoroutine(Begin());
+    }
+   
+    void Update()
+    {
+        if (!isDone && begin)
+        {
+            if (cg.alpha < 1)
+            {
+                cg.alpha += 0.05f;
+            }
+        }
+        if (isSkipped)
+        {
+            cg.alpha -= 0.05f;
+        }
+        if (canSkip)
+        {
+            if (Input.anyKeyDown)
+            {
+                StopAllCoroutines();
+                isSkipped = true;
+                StartCoroutine(Skipped());
+                canSkip = false;
+            }
+        }
+    }
+
+    public IEnumerator Begin()
+    {
+        yield return new WaitForSeconds(1);
+        begin = true;
+        yield return new WaitForSeconds(2);
+        isDone = true;
+        canSkip = true;
+        yield return new WaitForSeconds(5);
+        isSkipped = true;
+        yield return new WaitForSeconds(2);
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public IEnumerator Skipped()
+    {
+        yield return new WaitForSeconds(2);
+        SceneManager.LoadScene("MainMenu");
+    }
+}
