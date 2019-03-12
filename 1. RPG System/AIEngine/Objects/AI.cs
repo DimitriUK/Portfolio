20caef4229773fb2bdc521a1ee6ccc8099c381e6
Dimitri Zangana <dimitri@nregames.co.uk>
2019-03-12 18:33:56 +0200

    RPG System (AI Creation System)

---
+++ 1. RPG System/AIEngine/Objects/AI.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Enemy", menuName = "Enemy")]
+public class AI : ScriptableObject
+{
+    [Header("NPC Info")]
+    public new string name;
+    public string desc;
+    public Sprite faction;
+
+    [Header("NPC Stats")]
+    public int health;
+    public int stamina;
+    public int level;
+
+    [Header("NPC Type")]
+
+    public int race; 
+    public int fearLevel;
+
+
+    [Header("NPC Unlockables")]
+    public string[] learnedDesc;
+
+    /// <summary>
+    /// RACES:
+    /// 0: HUMAN
+    /// 1: DWARF
+    /// 2: UNDEAD
+    /// 3: ELVEN
+    /// 4: INFECTED HUMAN
+    /// 
+    /// FEAR LEVELS:
+    /// 0 = MOST BRAVE
+    /// 5 = MOST SCARED
+    /// </summary>
+}
---
+++ 1. RPG System/AIEngine/Scripts/AIDisplayer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AIDisplayer : MonoBehaviour
+{
+    public AI ai;
+
+    public new Text name;
+    public Text desc;
+
+    public Image faction;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        name.text = ai.name;
+        desc.text = ai.desc;
+        faction.sprite = ai.faction;
+    }
+}
